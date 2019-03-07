f03d730a357a6ff4ca6524481c624daab7548e84
Racing Race <got26days@gmail.com>
2019-03-07 10:42:19 +0300

    asdasd

---
+++ resources/views/pages/nav.blade.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg navbar-dark navbar__bluecolor">
+
+        <div class="logo-area">
+          <div class="logo-area__first">
+              <img src="/images/logo.png" alt="logo">
+          </div>
+          <div class="logo-area__second">
+              МОСКВА<br />
+              <span>
+                  07.03.2019, 08:13
+              </span>
+          </div>
+        </div>
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          
+        </button>
+      
+        <div class="collapse navbar-collapse new-flex-m" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto new-flex-m">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Шоу бизнес <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="delim"> | </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Политика</a>
+            </li>
+            <li class="delim"> | </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Новости</a>
+            </li>
+            <li class="delim"> | </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Оборона</a>
+            </li>
+            <li class="delim"> | </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Здоровье </a>
+            </li>
+            <li class="delim"> | </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Кухня</a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
