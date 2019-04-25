ee8c34713c030b82b4f5a0a18ba25e6655295c5f
Racing Race <got26days@gmail.com>
2019-04-25 09:53:35 +0300

    test4

--- routes/web.php
+++ routes/web.php
@@ -26,17 +26,14 @@ $subdomain = env('SECOND_DOMAIN');
 // Route::get('/', 'MainController@index')->name('manepage');
 
 Route::domain($domain)->group(function () {
-    Route::get('/change', 'MainController@change');
+    // Route::get('/change', 'MainController@change');
 
-
-    Route::get('/backpage', 'MainController@backpage');
     Route::get('/', 'MainController@index')->name('manepage');
+    Route::get('/backpage', 'MainController@backpage');
     Route::get('/politika', 'MainController@politika')->name('politika');;
     Route::get('/shou-biznes', 'MainController@shoubiznes')->name('shou-biznes');;
     Route::get('/zdorove', 'MainController@zdorove')->name('zdorove');;
     Route::get('/astrologiya', 'MainController@astrologiya')->name('astrologiya');;
-    // Route::get('/kuhniy', 'MainController@kuhniy');
-    
     Route::get('/post{id}', 'MainController@shou')->name('post');
     Route::get('/article', 'MainController@article');
     
