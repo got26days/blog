a5dae01cee779369e770b855cc5a0cd9ec6931e7
Sergey <got26days@gmail.com>
2019-11-01 12:35:53 +0300

    Change logic;

--- app/Item.php
+++ app/Item.php
@@ -23,41 +23,41 @@ class Item extends Model
          return $this->hasMany('App\Click');
      }
 
-     function scopeGetClickAttribute($firstdate, $seconddate)
-     {
-          $clicks = Clicks::chank(200)->where('item_id', '=', $this->id)->where('created_at', '>=', $firstdate)
-          ->where('created_at', '<=', $seconddate)->get();
-          $collect = collect($clicks);
-          $click  = $collect->sum('click');
+     // function scopeGetClickAttribute($firstdate, $seconddate)
+     // {
+     //      $clicks = Clicks::chank(200)->where('item_id', '=', $this->id)->where('created_at', '>=', $firstdate)
+     //      ->where('created_at', '<=', $seconddate)->get();
+     //      $collect = collect($clicks);
+     //      $click  = $collect->sum('click');
 
-          return $click;
-     }
+     //      return $click;
+     // }
 
-     public function scopeGetViewAttribute($firstdate, $seconddate)
-     {
-          $clicks = Clicks::chank(200)->where('item_id', '=', $this->id)->where('created_at', '>=', $firstdate)
-          ->where('created_at', '<=', $seconddate)->get();
-          $collect = collect($clicks);
-          $click  = $collect->sum('view');
+     // public function scopeGetViewAttribute($firstdate, $seconddate)
+     // {
+     //      $clicks = Clicks::chank(200)->where('item_id', '=', $this->id)->where('created_at', '>=', $firstdate)
+     //      ->where('created_at', '<=', $seconddate)->get();
+     //      $collect = collect($clicks);
+     //      $click  = $collect->sum('view');
 
-          return $click;
-     }
+     //      return $click;
+     // }
 
-     function scopeGetCtrAttribute($firstdate, $seconddate)
-     {
-          $clicks = $this->clicks->chunk(200)->where('created_at', '>=', $firstdate)
-          ->where('created_at', '<=', $seconddate);
+     // function scopeGetCtrAttribute($firstdate, $seconddate)
+     // {
+     //      $clicks = $this->clicks->chunk(200)->where('created_at', '>=', $firstdate)
+     //      ->where('created_at', '<=', $seconddate);
 
-          $collect = collect($clicks);
+     //      $collect = collect($clicks);
             
-          $click  = $collect->sum('click');
-          $view =  $collect->sum('view');
+     //      $click  = $collect->sum('click');
+     //      $view =  $collect->sum('view');
 
-          if($view == 0){
-               $view = 1;
-          }
-          $result = floatval($click)/floatval($view);
-          $result = number_format((float)$result * 100, 4, '.', '');
-          return $result;
-     }
+     //      if($view == 0){
+     //           $view = 1;
+     //      }
+     //      $result = floatval($click)/floatval($view);
+     //      $result = number_format((float)$result * 100, 4, '.', '');
+     //      return $result;
+     // }
 }
